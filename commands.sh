1ec8413fef93b02582b306f76bf1b34a8080234f
boyoss <yossef68e@gmail.com>
2021-10-01 01:05:43 +0300

    personal change

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 
-docker-compose -f /mnt/remoteAsterisk/AGI/scripts/alpsteriskSimple/docker-compose.yml up --build -d
+docker-compose  up --build -d
 docker exec -it alpbxSimple sh
 
 netstat -ntulp | grep 4060
